b3b82fe1be6ed9009310b72ff51928e9ff8464ff
liangjiangtao <liangjiangtao@hongyuanshidai.cn>
2018-05-09 19:14:15 +0800

    init

---
+++ douban.py
@@ -0,0 +1,52 @@
+import json
+
+import requests
+
+
+#  测试爬虫，抓取gank数据
+
+
+class GankScrapy:
+
+    gank_url = "http://gank.io/api/data/福利/15/1"
+    offset = 0
+
+
+    error = ''
+
+
+
+    def crawl(self, params=None):
+        # UA 设置。。
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
+        }
+        response = requests.get(self.gank_url, headers=headers)
+        print("url = "+response.url)
+
+        print( 'resp_code = '+ str(response.status_code))
+
+
+        myBody = json.loads(json.dumps(response.json()))
+
+        print("mybody = " + str(myBody))
+
+        print("myBody 的类型是 = " + str(type(myBody)))
+
+        for key1 in myBody:
+            print (key1 +":" + str(myBody[key1]))
+
+        self.save(response.json())
+
+
+
+
+    def save(self, data):
+        with open("gank.txt", "a", encoding="utf-8") as f:
+                f.write(json.dumps(data))
+
+
+
+if __name__ == '__main__':
+    GankScrapy().crawl()
---
+++ zhihu_test.py
@@ -0,0 +1,42 @@
+import json
+
+import requests
+
+
+#  测试爬虫，抓取知乎数据
+
+
+class TestScrapy:
+
+    init_url = "https://zhuanlan.zhihu.com/api/columns/pythoneer/followers"
+    offset = 0
+
+    def crawl(self, params=None):
+        # UA 设置。。
+        headers = {
+            "Host": "zhuanlan.zhihu.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
+        }
+        response = requests.get(self.init_url, headers=headers, params=params)
+        print(response.url)
+        data = response.json()
+
+        while self.offset < 100:
+            self.parse(data)
+            self.offset += 20
+            params = {"limit": 20, "offset": self.offset}
+            self.crawl(params)
+
+
+
+    def parse(self, data):
+    # 以json格式存储到文件
+        with open("followers.json", "a", encoding="utf-8") as f:
+            for item in data:
+                f.write(json.dumps(item))
+                f.write('\n')
+
+
+if __name__ == '__main__':
+    TestScrapy().crawl()
